7da362f7097a96b3b55c8e1dac49915e18101b96
Victor Blinov <blinov.01@mail.ru>
2021-06-25 23:04:47 +0300

    parser_6

--- parser.py
+++ parser.py
@@ -1,10 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 URL = 'https://lka.mgimo.ru/upload/sp/spospi%20(HTML).html'
+
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0', 'accept':
 	'*/*' }
 #Заголовки - объявляем их для того, чтобы сайт не принял нас за бота.
+
+FILE = 'Список_колледж.csv'
 
 def get_html(url, params=None):
     r = requests.get(url, headers=HEADERS, params=params)
@@ -22,13 +26,20 @@ def get_content(html):
     for item in items:
         if (item.find('td', class_='R12C2') != None):
             applicants.append({
-                'Applicant name': item.find('td', class_='R12C2').get_text(strip = True),
+                'ФИО': item.find('td', class_='R12C2').get_text(strip = True),
                 #strip - убирание пробелов спереди и сзади, get_text - получение текста только между тегами
-                'Average score': item.find('span').get_text(strip = True)
+                'Средний_балл': item.find('span').get_text(strip = True)
             })
 
-    print(applicants)
-    print(len(applicants))
+    return applicants
+
+def save_file(items, path):
+    with open(path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(['ФИО', 'Средний балл'])
+        for item in items:
+            writer.writerow([item['ФИО'], item['Средний_балл']])
+
 
 def parse():
     html = get_html(URL)
@@ -36,7 +47,10 @@ def parse():
     #print(html.status_code)
     #Выводим ответ от сайта, если 200 - то всё ок.
     if html.status_code == 200:
-        get_content(html.text)
+        applicants = get_content(html.text)
+        save_file(applicants, FILE)
+        print(applicants)
+        print("Количество абитуриентов: " + str(len(applicants)))
     else:
         print("Ошибка!")
 
